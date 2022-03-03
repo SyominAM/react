10016c2d481461e078e8fc9b0e5c67fc694673af
SyominAM <seomin.aleksander2015@yandex.ru>
2022-03-04 00:31:57 +0300

    create 'state'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import './App.css';
 import React, {Component} from 'react';
 import Car from './Car/Car';
 class App extends Component{
+  state ={
+    cars: [
+      {name: 'KIA',year:2108},
+      {name: 'FORD',year:2118},
+    ],
+    pageTitle:'React components'
+  }
+  
+ 
   render(){
     const divStyle = {
       'color': 'orange',
       'text-align':'center'
     }
+
+    const cars = this.state.cars
     return(
       <div style = {divStyle}>
+        <h1>{this.state.pageTitle}</h1>
         <h1  style={{fontSize: '220px',margin: 'auto'}}>Hello, stranger!</h1>
-        <Car name = {'BMW'} year = {2228} />
-          <p style ={{color:'red'}}>:D (chld)</p>
-        <Car name = {'KIA'} year = {1234} />
+        <Car name = {cars[0].name} year = {cars[0].year}/>
+        <Car name = {cars[1].name} year = {cars[1].year}/>
       </div>
     );
   }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -5,6 +5,5 @@ export default (props) =>(
     <div>
         <h1>Car name: {props.name} </h1>
         <h1>year:{props.year}  </h1>
-        {props.children}
     </div>
 )
